06c0e9539fb01a95be882f050925137bf0c8b472
wanjiku <annritawanjiku77@gmail.com>
2023-06-06 13:23:33 +0300

    initialised project and setup react router dom

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+
+
+
+  
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Home/>
+    },
+    {
+      path: "/products/:id",
+      element: <Products/>
+    },
+    {
+      path: "/product/:id",
+      element: <Product/>
+    },
+
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
