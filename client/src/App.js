d103482cba2c96b521754f7712b5ed4bdde85be7
wanjiku <annritawanjiku77@gmail.com>
2023-06-06 13:37:22 +0300

    added outlet for nav and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,46 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
+  Outlet,
   } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
 
 
+const Layout = () =>{
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
 
+  )
+}
   
 const router = createBrowserRouter([
     {
       path: "/",
-      element:<Home/>
-    },
-    {
-      path: "/products/:id",
-      element: <Products/>
-    },
-    {
-      path: "/product/:id",
-      element: <Product/>
-    },
+      element:<Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path:"/products/:id",
+          element: <Products/>
+        },
+        {
+          path: "/product/:id",
+          element: <Product/>
+        },
+      ]
+    }
+
 
 ]);
 
