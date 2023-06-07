e8b4f8d64a3623c21866567e3516f3907493ddf5
wanjiku <annritawanjiku77@gmail.com>
2023-06-07 06:52:30 +0300

    added navbar with mui icons, and sass styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,67 @@
 import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from 'react-router-dom';
+import "./Navbar.scss"
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='wrapper'>
+
+        <div className='left'>
+          <div className='item'>
+            <img src='/img/flagicon.png' alt=''/>
+            <KeyboardArrowDownIcon/>
+          </div>
+          <div className='item'>
+            <span>USD</span>
+            <KeyboardArrowDownIcon/>
+          </div>
+          <div className='item'>
+            <Link className='link'  to="/products/1">Women</Link>
+          </div>
+          <div className='item'>
+            <Link className='link'  to="/products/2">Men</Link>
+          </div>
+          <div className='item'>
+            <Link  className='link' to="/products/3">Children</Link>
+          </div>
+        </div>
+
+        <div className='center'>
+          <Link className='link'  to="/">VIVOSTORE</Link>
+        </div>
+
+        <div className='right'>
+          <div className='item'>
+              <Link className='link' to="/">Homepage</Link>
+          </div>
+          <div className='item'>
+              <Link className='link' to="/about">About</Link>
+          </div>
+          <div className='item'>
+              <Link className='link' to="/contact">Contact</Link>
+          </div>
+          <div className='item'>
+              <Link className='link' to="/stores">Stores</Link>
+          </div>
+          <div className='icons'>
+            <SearchIcon/>
+            <PersonOutlineIcon/>
+            <FavoriteBorderOutlinedIcon/>
+            <div className='cartIcon'>
+              <ShoppingCartOutlinedIcon/>
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
